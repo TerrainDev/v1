7d0d2d33dd72bebaa5078f159b20bae7bfb6a6c9
Javier Szyfer <jszyfer@gmail.com>
2021-11-11 08:37:02 -0300

    sdgsd

--- pages/store.js
+++ pages/store.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-blue-200">
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 ">
-        <h1 className="text-6xl font-bold">Store </h1>
+        <h1 className="text-6xl font-bold">Storet t </h1>
       </main>
     </div>
   );
