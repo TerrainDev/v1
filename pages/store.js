476c0b329a43ac8a1ddf82dcab581402a9cb66f5
Javier Szyfer <jszyfer@gmail.com>
2021-11-11 08:19:37 -0300

    test

---
+++ pages/store.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-blue-200">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 ">
+        <h1 className="text-6xl font-bold">Terrain - Store </h1>
+      </main>
+    </div>
+  );
+}
